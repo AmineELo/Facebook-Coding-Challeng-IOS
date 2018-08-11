564aa2d36c1b7922b931cfdb569bc54636279196
Amine Elouattar <elouattar@gmail.com>
2018-08-11 23:27:35 +0100

    initial commit

---
+++ Facebook Coding Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook Coding Challenge
+//
+//  Created by Amine Elouattar on 8/3/18.
+//  Copyright © 2018 Amine Elouattar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'Facebook Coding Challenge' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Facebook Coding Challenge
+    pod 'FacebookCore'
+    pod 'FacebookLogin'
+    pod 'FacebookShare'
+
+end
