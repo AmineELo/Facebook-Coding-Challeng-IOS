748ee7655826ce98ac58fdd81995ce4ff2a90799
Amine Elouattar <elouattar@gmail.com>
2018-08-12 00:32:18 +0100

    add login functionality to the LoginViewController.

--- Facebook Coding Challenge/ViewController.swift
+++ Facebook Coding Challenge/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let accessToken = AccessToken.current {
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        loginWithFacebook()
+    }
+    
+    func loginWithFacebook(){
+        let loginManager = LoginManager()
+        
+        loginManager.logIn(readPermissions: [.publicProfile], viewController: self) { (loginResult) in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(_, _, _):
+                print("Logged in!")
+            }
+        }
+    }
+    
+    
 }
 
