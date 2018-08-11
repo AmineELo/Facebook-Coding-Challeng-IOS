d46d0f0a56eeaa5fa42b8c8b56866330ece5e2b8
Amine Elouattar <elouattar@gmail.com>
2018-08-12 00:43:21 +0100

    Add Albums grid view controller.

---
+++ Facebook Coding Challenge/AlbumsGridViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumsGridViewController.swift
+//  Facebook Coding Challenge
+//
+//  Created by Amine Elouattar on 8/12/18.
+//  Copyright © 2018 Amine Elouattar. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
