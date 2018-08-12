1d058e0ee04571d6eebbe7344378d7f2ffce5d76
Amine Elouattar <elouattar@gmail.com>
2018-08-12 11:09:02 +0100

    add picture model and conforme to the UICollectionview protocoles.

--- Facebook Coding Challenge/Controller/PictureViewController.swift
+++ Facebook Coding Challenge/Controller/PictureViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import Alamofire
+import AlamofireImage
 
 class PictureViewController: UIViewController {
     
+    @IBOutlet weak var pictureCollectionView: UICollectionView!
     var albumId = ""
+    
+    var pictures = [Picture]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print(albumId)
+    }
+}
+
+extension PictureViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        return UICollectionViewCell()
     }
 }
