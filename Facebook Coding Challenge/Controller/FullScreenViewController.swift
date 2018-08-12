56ebd45bf6396a0c07d0f8c129b420f55436d6c6
Amine Elouattar <elouattar@gmail.com>
2018-08-12 11:33:52 +0100

    Create a full screen view Controller.

---
+++ Facebook Coding Challenge/Controller/FullScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FullScreenViewController.swift
+//  Facebook Coding Challenge
+//
+//  Created by Amine Elouattar on 8/12/18.
+//  Copyright © 2018 Amine Elouattar. All rights reserved.
+//
+
+import UIKit
+
+class FullScreenViewController: UIViewController {
+
+    @IBOutlet weak var fullScreenImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
