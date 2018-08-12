060adbfb58cc4b713dbbcac918ff96c825ab3233
Amine Elouattar <elouattar@gmail.com>
2018-08-12 10:16:08 +0100

    add image view and download its content.

--- Facebook Coding Challenge/AlbumsGridViewController.swift
+++ Facebook Coding Challenge/AlbumsGridViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import FacebookCore
 import FacebookLogin
+import Alamofire
+import AlamofireImage
 
 class AlbumsGridViewController: UIViewController {
 
@@ -31,8 +33,8 @@ class AlbumsGridViewController: UIViewController {
     func configureAlbumGrid(){
         albumCollectionView.delegate = self
         albumCollectionView.dataSource = self
-        // make the cell width equal to half the screen (-20 for the spacing)
-        let width = (view.frame.size.width - 20) / 2
+        // make the cell width equal to half the screen (-10 for the spacing)
+        let width = (view.frame.size.width - 10) / 2
         let layout = albumCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
     }
@@ -81,18 +83,31 @@ class AlbumsGridViewController: UIViewController {
         if let responseDictionary = response.dictionaryValue{
             if let data: NSArray = responseDictionary["data"] as? NSArray{
                 for  i in 0..<data.count {
-                    let valueDict : NSDictionary = data[i] as! NSDictionary
-                    let count = valueDict.object(forKey: "count") as! NSNumber
-                    let id = valueDict.object(forKey: "name") as! String
-                    let pictureUrl = ((valueDict.object(forKey: "picture") as! NSDictionary)
-                        .object(forKey: "data") as! NSDictionary)
-                        .object(forKey: "url") as! String
-                    print(count)
-                    print(id)
-                    print(pictureUrl)
+                    appendAlbum(array: data, index: i)
                 }
+                albumCollectionView.reloadData()
             }
         }
+    }
+    
+    func appendAlbum(array: NSArray, index: Int){
+        //Extract the an object from the data array
+        let valueDict : NSDictionary = array[index] as! NSDictionary
+        
+        let id = valueDict.object(forKey: "id") as! String
+        let count = valueDict.object(forKey: "count") as! NSNumber
+        let name = valueDict.object(forKey: "name") as! String
+        let pictureUrl = ((valueDict.object(forKey: "picture") as! NSDictionary)
+            .object(forKey: "data") as! NSDictionary)
+            .object(forKey: "url") as! String
+        // append data to the dataset
+        let album = Album()
+        album.albumName = name
+        album.albumCoverUrl = pictureUrl
+        album.albumCount = String(count as! Int)
+        album.albumId = id
+        
+        albums.append(album)
     }
 }
 
@@ -103,7 +118,16 @@ extension AlbumsGridViewController: UICollectionViewDelegate, UICollectionViewDa
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = albumCollectionView.dequeueReusableCell(withReuseIdentifier: "albumCollectionCell", for: indexPath)
+        if let nameLabel = cell.viewWithTag(10) as? UILabel{
+            nameLabel.text = albums[indexPath.row].albumName
+        }
         
+        //Image downloading
+        if let coverImage = cell.viewWithTag(20) as? UIImageView{
+            let url = URL(string: albums[indexPath.row].albumCoverUrl)
+            coverImage.af_setImage(withURL: url!)
+        }
+
         return cell
     }
 }
